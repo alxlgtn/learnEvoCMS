5d95c1c71e3e4e169b0a972d542cf68e8f3363a5
alxlgtn <muteproj@gmail.com>
2020-12-09 16:21:16 +0300

    delete files

--- views/partials/part_footer--scroll_to_top.blade.php
+++
@@ -1,2 +0,0 @@
-<div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i></i></a></div>
-<input name="animation" type="hidden">
--- views/partials/successTpl.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="thankyou">
-    <h4 class="red">Ваш вопрос успешно отправлен!</h4>
-    <div>
-        <span class="link__arrow">
-            <span class="link link__underline">Спросить ещё</span>
-        </span>
-    </div>
-</div>
